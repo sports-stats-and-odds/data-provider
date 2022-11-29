722b89a79dff136530fceaeab03628043acb9856
Matteo DE VITA PC <matteo.de-vita@epitech.eu>
2022-11-29 19:38:00 +0100

    add fbref basic scraping

--- dataprovider/fbref_scrapper.py
+++ dataprovider/fbref_scrapper.py
@@ -15,16 +15,36 @@ class FbrefScrapper(ScrapperInterface):
             date = date + timedelta(days=1)
             return date.strftime("%Y-%m-%d")
     
-        currentDateStr: str = "2011-01-02"
+        currentDateStr: str = "2022-11-25"
 
-        while datetime.strptime(currentDateStr, "%Y-%m-%d") < datetime.today(): #while currentDate < todayDate
-            print(f'currentDateStr : {currentDateStr}')
-            self.baseUrl += currentDateStr
-            # req = requests.get(self.baseUrl)
-            # soup = BeautifulSoup(req.content, 'html.parser')
-            # tables = soup.find_all('div', {"class": "table_wrapper tabbed"})
-            currentDateStr = getNextDateStr(currentDateStr)
-        # print(tables[0].prettify())
+        # while datetime.strptime(currentDateStr, "%Y-%m-%d") < datetime.today() and False: #while currentDate < todayDate
+        print(f'currentDateStr : {currentDateStr}')
+        self.baseUrl += currentDateStr
+        req = requests.get(self.baseUrl)
+        soup = BeautifulSoup(req.content, 'html.parser')
+        tables = soup.find_all('div', {"class": "table_wrapper tabbed"})
+        for table in tables:
+            rows = table.find_all('tr')
+            rows.pop(0) #remove first row because it is the table header
+            for row in rows:
+                time = row.find('span', {"class": 'venuetime'}).text
+                homeTeam = row.find('td', {"data-stat": 'home_team'}).find('a').text
+                awayTeam = row.find('td', {"data-stat": 'away_team'}).find('a').text
+                splittedScore = row.find('td', {"data-stat": 'score'}).find('a').text.split("–") #this is noty a basic dash : the charatcter is "–" whereas a basic dash is "-"
+                homeTeamScore = splittedScore[0]
+                awayTeamScore = splittedScore[1]
+                attendance = row.find('td', {"data-stat": 'attendance'}).text
+                referee = row.find('td', {"data-stat": 'referee'}).text
+                print(f"date = {currentDateStr}")
+                print(f"time = {time}")
+                print(f"homeTeam = {homeTeam}")
+                print(f"awayTeam = {awayTeam}")
+                print(f"homeTeamScore = {homeTeamScore}")
+                print(f"awayTeamScore = {awayTeamScore}")
+                print(f"attendance = {attendance}")
+                print(f"referee = {referee}")
+            # currentDateStr = getNextDateStr(currentDateStr)
+            # print(tables[0].prettify())
 
 temp = FbrefScrapper()
 temp.scrap()
